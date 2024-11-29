5290963f98b929d93d54ca44fef6ad41849f782e
MaNicolas <48954140+MaNicolas@users.noreply.github.com>
2024-11-29 13:32:10 +0100

    chore: Adds .env file to get rid of hard coded values

--- src/step-definitions/hooks/hooks.ts
+++ src/step-definitions/hooks/hooks.ts
@@ -1,8 +1,48 @@
 import { BeforeAll, Before, AfterAll, After, Status } from "@cucumber/cucumber";
-import { Browser, chromium } from "@playwright/test";
+import { Browser, BrowserType, chromium, firefox, webkit } from "@playwright/test";
 import { pageFixture } from "./browserContextFixture";
 
-let browser: Browser;
+//Loads env variables from .env file
+import { config as loadEnv } from "dotenv";
+const env = loadEnv({ path: "./env/.env" });
+
+//Create a configuration object for easy access to env variables
+const config = {
+  headless: env.parsed?.HEADLESS === 'true',
+  browser: env.parsed?.UI_AUTOMATION_BROWSER || 'chromium',
+  width: parseInt(env.parsed?.BROWSER_WIDTH || '1920'),
+  height: parseInt(env.parsed?.BROWSER_HEIGHT || '1080')
+};
+
+//Create dictionnary mapping browser names to their launch functions
+const browsers: {[key: string]: BrowserType} = {
+  'chromium': chromium,
+  'firefox': firefox,
+  'webkit': webkit
+}
+
+let browserInstance: Browser | null = null;
+
+async function initializeBrowserContext(selectedBrowser: string): Promise<Browser>{
+  const launchBrowser = browsers[selectedBrowser];
+  if(!launchBrowser)
+  {
+    throw new Error(`Invalid browser selected: ${selectedBrowser}`);
+  }
+
+  return await launchBrowser.launch({headless: config.headless});
+}
+
+async function initializePage(): Promise<void>{
+  if(!browserInstance){
+    throw new Error ('Browser instance is null');
+  }
+
+  pageFixture.context = await browserInstance.newContext({ignoreHTTPSErrors: true});
+
+  pageFixture.page = await pageFixture.context.newPage();
+  await pageFixture.page.setViewportSize({width: config.width, height: config.height});
+}
 
 BeforeAll(async function () {
   console.log("\nExecuting test suite...");
@@ -13,11 +53,12 @@ AfterAll(async function () {
 });
 
 Before(async function () {
-  browser = await chromium.launch({ headless: false });
-  pageFixture.context = await browser.newContext({
-    viewport: { width: 1920, height: 1080 },
-  });
-  pageFixture.page = await pageFixture.context.newPage();
+  try {
+    browserInstance = await initializeBrowserContext(config.browser);
+    await initializePage();
+} catch (error) {
+  console.error('Browser context initialization failed:', error);
+}
 });
 
 After(async function ({ pickle, result }) {
@@ -36,6 +77,8 @@ After(async function ({ pickle, result }) {
     }
   }
 
-  await pageFixture.context.close();
-  await browser.close();
+  if(browserInstance){
+    await pageFixture.page?.close();
+    await browserInstance.close();
+  }
 });
